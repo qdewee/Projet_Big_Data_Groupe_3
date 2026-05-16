69296e3cf1d72d8734f65e24ab172c20cafd088b
amarchalnamur <arnauldmarchal@gmail.com>
2026-05-16 19:30:08 +0200

    Create collection of well-rated films from ratings
    This script creates a collection of well-rated films by aggregating ratings, filtering for an average rating greater than 4.5, and joining with the movies collection.

---
+++ Projet/MongoDB/requete 2.js
@@ -0,0 +1,70 @@
+//création collection films_bien_notes
+//Temps d'exécution : 729235 ms
+
+use movies_project
+
+db.ratings_clean.createIndex({ movie_id: 1 });
+db.movies_clean.createIndex({ id: 1 });
+
+const start = Date.now();
+
+db.ratings_clean.aggregate([
+  {
+    $group: {
+      _id: "$movie_id",
+      note_moyenne: { $avg: "$rating" },
+      nombre_notes: { $sum: 1 }
+    }
+  },
+  {
+    $match: {
+      note_moyenne: { $gt: 4.5 }
+    }
+  },
+  {
+    $lookup: {
+      from: "movies_clean",
+      localField: "_id",
+      foreignField: "id",
+      as: "movie"
+    }
+  },
+  {
+    $unwind: "$movie"
+  },
+  {
+    $project: {
+      _id: 0,
+      movie_id: "$_id",
+      title: "$movie.title",
+      year: "$movie.year",
+      director_id: "$movie.director_id",
+      note_moyenne: { $round: ["$note_moyenne", 2] },
+      nombre_notes: 1
+    }
+  },
+  {
+    $sort: {
+      note_moyenne: -1,
+      nombre_notes: -1
+    }
+  },
+  {
+    $out: "films_bien_notes"
+  }
+]);
+
+const end = Date.now();
+
+print("Collection films_bien_notes créée avec succès.");
+print("Temps d'exécution : " + (end - start) + " ms");
+
+db.films_bien_notes.createIndex({ note_moyenne: -1 });
+db.films_bien_notes.createIndex({ title: 1 });
+
+
+
+//Code pour la lancer la requête qui retourne tous les films avec une note > 4.5 avec : titre, année, ID et directeur
+
+
+
